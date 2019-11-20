efbf8ace900dd2bad99100a2b37191b30c946ec0
little-little-fox <3091953930@qq.com>
2019-11-20 16:40:46 +0800

    对 登录、注册、个人中心、规则页面 重命名

--- XB/pages/login/login.js
+++ XB/pages/login/login.js
@@ -1,91 +1,61 @@
-// pages/login/login.js
-const ctx = wx.createCanvasContext("firstCanvas")//the context of Canvas 
+// pages/index2/index2.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
- 
+    id: " ",
+    passward: " "
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {    
-   // var loginFrame = this.roundRect(40, 130, 240, 190,30)
-  //  ctx.draw()
-    this.roundRect(40, 130, 240, 190, 30)
-    this.font()
-    
-    //this.button()
-    ctx.draw()
-
-  
-  },
- 
-  /**
- * 绘制圆角矩形
- * @param {Object} ctx - canvas组件的绘图上下文
- * @param {Number} x - 矩形的x坐标
- * @param {Number} y - 矩形的y坐标
- * @param {Number} w - 矩形的宽度
- * @param {Number} h - 矩形的高度
- * @param {Number} r - 矩形的圆角半径
- * @param {String} [c = 'transparent'] - 矩形的填充色
- */
-  roundRect: function (x, y, w, h, r, c = 'rgba(208,233,225, 0.8)') {
-    if (w < 2 * r) { r = w / 2; }
-    if (h < 2 * r) { r = h / 2; }
-
-    ctx.beginPath();
-    ctx.fillStyle = c;
-  
-    ctx.arc(x + r, y + r, r, Math.PI, Math.PI * 1.5);
-    ctx.moveTo(x + r, y);
-    ctx.lineTo(x + w - r, y);
-    ctx.lineTo(x + w, y + r);
-
-    ctx.arc(x + w - r, y + r, r, Math.PI * 1.5, Math.PI * 2);
-    ctx.lineTo(x + w, y + h - r);
-    ctx.lineTo(x + w - r, y + h);
-
-    ctx.arc(x + w - r, y + h - r, r, 0, Math.PI * 0.5);
-    ctx.lineTo(x + r, y + h);
-    ctx.lineTo(x, y + h - r);
-
-    ctx.arc(x + r, y + h - r, r, Math.PI * 0.5, Math.PI);
-    ctx.lineTo(x, y + r);
-    ctx.lineTo(x + r, y);
-    ctx.fill();
-    ctx.closePath();
-    
+  onReady: function () {
   },
 
-  font : function(){
-    ctx.font = '11px bold 黑体';
-    ctx.fillStyle = "#000000";
-    ctx.textAlign = "left";
+/*获取输入的学号 */
+  getId : function(e){
+    this.setData({
+      id: e.detail.value
+    })
    
-    ctx.setStrokeStyle('black');
-
-    ctx.fillText("请使用OUC教务处密码登录", 80, 250);
+    console.log("id:" + this.data.id)
 
   },
+/*获取输入的密码 */
+  getPassward: function(e){
+    this.setData({
+      passward: e.detail.value
+    })
+   
+    console.log("passward:" + this.data.passward)
+  },
 
-  button : function(){
-    var btnQR = new button();
-    btnQR.Content = "确认";
-    ctx.AddVisual(btn);
-    ctx.SetLeft(btn, 100);
-    ctx.SetTop(btn, 200); 
+  userSubmit: function(){
+   var userId = this.data.id
+   console.log(userId)
+   var userPwd = this.data.passward
+   console.log(userPwd)
+  },
 
+  adminSubmit: function(){
+    var that = this;
+    wx.request({
+      url: 'xxxxxxxxxxxxxxx', /*提交地址？？？ */
+      data: {
+        /*token: '7b40b56eb2e27195dfcdcc3322312a4eb67229a000000015ea84b8657',*/
+        adminId:this.data.id,
+        adminPwd: this.data.passward
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        that.successRemind();
+      }
+    })
+   
   }
 })
