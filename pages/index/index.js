04127c162ebec8b81fead5d2ddd2886e5f8e2d1d
Nisces <Nisces0304@outlook.com>
2019-11-23 11:28:53 +0800

    very big change

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,32 +1,32 @@
 // pages/index/index.js
-const utils = require('../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    utils.alert(utils.formatTime(new Date()));
+
   },
 
   /**
@@ -62,5 +62,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
+  }
 })
