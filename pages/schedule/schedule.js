aaae795902b279992686357a0f2f10279d36810d
Nisces <Nisces0304@outlook.com>
2019-12-20 14:30:35 +0800

    时刻表基本完成

--- pages/schedule/schedule.js
+++ pages/schedule/schedule.js
@@ -1,48 +1,82 @@
 // pages/schedule/schedule.js
 const utils = require('../../utils/util.js');
-var start = -1;
+var index = 0;
 var count = 0;
 
-function init(schedule) {
-  for (let i = 10; i < 15; i++) {
+function init() {
+  var schedule = new Array(7);
+  for (let i = 0; i < 7; i++) {
+    schedule[i] = new Array(30);
+    for (let j = 0; j < 30; j++) {
+      schedule[i][j] = "";
+    }
+  }
+  for (var i = 10; i < 15; i++) {
     schedule[0][i] = "occupied";
   }
+  schedule[0][i] = "occupied last";
 
-  for (let i = 13; i < 20; i++) {
+  for (var i = 13; i < 20; i++) {
     schedule[2][i] = "confirmed";
   }
+  schedule[2][i] = "confirmed last";
 
-  for (let i = 4; i < 10; i++) {
+  for (var i = 4; i < 10; i++) {
     schedule[4][i] = "confirmed";
   }
+  schedule[4][i] = "confirmed last";
 
-  for (let i = 11; i < 17; i++) {
+  for (var i = 11; i < 17; i++) {
     schedule[6][i] = "occupied";
   }
+  schedule[6][i] = "occupied last";
+  return schedule;
 }
+
+function isSelected(cell) {
+  return cell == "selected" || cell == "selected last";
+}
+
+function getStart() {
+  var start = [];
+  if (count == 0) {
+    start = ["00", "00"];
+    return start;
+  }
+  start[0] = ("0" + (parseInt(index / 2) + 8)).slice(-2);  //0+时间，取后两位，用于不足位数补0
+  start[1] = index % 2 == 0 ? "00" : "30";
+  return start;
+}
+
+function getEnd() {
+  var end = [];
+  if (count == 0) {
+    end = ["00", "00"];
+    return end;
+  }
+  var eIndex = index + count;
+  end[0] = ("0" + (parseInt(eIndex / 2) + 8)).slice(-2);  //0+时间，取后两位，用于不足位数补0
+  end[1] = eIndex % 2 == 0 ? "00" : "30";
+  return end;
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    schedule: []
+    schedule: [],
+    start: ["00", "00"],
+    end: ["00", "00"]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var array = new Array(7);
-    for (let i = 0; i < 7; i++) {
-      array[i] = new Array(30);
-      for (let j = 0; j < 30; j++) {
-        array[i][j] = "";
-      }
-    }
-    init(array);
     this.setData({
-      schedule: array
+      schedule: init()
     });
   },
 
@@ -99,26 +133,34 @@ Page({
     var col = e.currentTarget.dataset.col;
     var row = e.currentTarget.dataset.row;
     var schedule = this.data.schedule;
+    var start = this.data.start;
+    var end = this.data.end;
     if (count != 0) {
-      if (schedule[col][row + 1] != "selected" && schedule[col][row + 1] != "selected last" &&
-        schedule[col][row - 1] != "selected" && schedule[col][row - 1] != "selected last" &&
-        schedule[col][row] != "selected" && schedule[col][row] != "selected last") {
+      if (!isSelected(schedule[col][row + 1]) && !isSelected(schedule[col][row - 1]) &&
+        !isSelected(schedule[col][row])) {
         utils.alert("请选择连续的时间段！");
         return;
       }
     } else {
-      start = row;
+      index = row;
     }
     if (schedule[col][row] == "") {
       schedule[col][row] = "selected";
-      if (schedule[col][row + 1] != "selected" && schedule[col][row + 1] != "selected last") {
+      if (!isSelected(schedule[col][row + 1])) {
         schedule[col][row] += " last";
       }
       if (schedule[col][row - 1] == "selected last") {
         schedule[col][row - 1] = "selected";
       }
+      if (!isSelected(schedule[col][row - 1]) && isSelected(schedule[col][row + 1])) {
+        index--;
+      }
       count++;
-    } else if (schedule[col][row] == "selected" || schedule[col][row] == "selected last") {
+    } else if (isSelected(schedule[col][row])) {
+      if ((isSelected(schedule[col][row + 1])) && isSelected(schedule[col][row - 1])) {
+        utils.alert("请选择连续的时间段！");
+        return;
+      }
       schedule[col][row] = "";
       if (schedule[col][row - 1] == "selected") {
         schedule[col][row - 1] += " last";
@@ -129,11 +171,32 @@ Page({
       return;
     }
     this.setData({
-      schedule: schedule
+      schedule: schedule,
+      start: getStart(),
+      end: getEnd()
     });
   },
 
   submit: function(e) {
-    console.log(start, count);
+    if(count == 0) {
+      utils.alert("请选择时间！");
+    }
+    var start = this.data.start;
+    var end = this.data.end;
+    var startTime = start[0] + ":" + start[1];
+    var endTime = end[0] + ":" + end[1];
+    wx.navigateTo({
+      url:"/pages/application/application?start='"
+    })
+  },
+
+  reset:function(e) {
+    index = 0;
+    count = 0;
+    this.setData({
+      schedule: init(),
+      start:["00", "00"],
+      end:["00", "00"]
+    });
   }
 })
