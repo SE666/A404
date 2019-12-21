e026d4a8a52d42e54514dc95676645193a42ef3c
wangsenouc <48779115+wangsenouc@users.noreply.github.com>
2019-12-21 16:11:56 +0800

    Add files via upload

--- pages/adminSelect/adminSelect.js
+++ pages/adminSelect/adminSelect.js
@@ -1,4 +1,4 @@
-var josn = '[{ "id": 1, "stuid": "17020031068", "username": "汪wqs", "phone": "17852417737", "applydate": "2019-10-31", "start": "08:10", "end": "09:10", "number": 5, "ifmedia": "否", "reason": "****", "status": "待审核", "submitdatetime": "2019-10-31 08:11:15" }, { "id": 2, "stuid": "17020031068", "username": "汪wqs", "phone": "17852417737", "applydate": "2019-10-31", "start": "09:45", "end": "10:45", "number": 7, "ifmedia": "是", "reason": "####", "status": "审核通过", "submitdatetime": "2019-10-31 08:22:40" }, { "id": 3, "stuid": "17020031068", "username": "汪wqs", "phone": "17852417737", "applydate": "2019-10-31", "start": "09:45", "end": "10:45", "number": 7, "ifmedia": "是", "reason": "####", "status": "未还钥匙", "submitdatetime": "2019-10-31 08:22:40" }, { "id": 4, "stuid": "17020031068", "username": "汪wqs", "phone": "17852417737", "applydate": "2019-10-31", "start": "09:45", "end": "10:45", "number": 7, "ifmedia": "是", "reason": "####", "status": "未通过", "submitdatetime": "2019-10-31 08:22:40"}]'
+// var josn = '[{ "id": 1, "stuid": "17020031068", "username": "汪wqs", "phone": "17852417737", "applydate": "2019-10-31", "start": "08:10", "end": "09:10", "number": 5, "ifmedia": "否", "reason": "****", "status": "待审核", "submitdatetime": "2019-10-31 08:11:15" }, { "id": 2, "stuid": "17020031068", "username": "汪wqs", "phone": "17852417737", "applydate": "2019-10-31", "start": "09:45", "end": "10:45", "number": 7, "ifmedia": "是", "reason": "####", "status": "审核通过", "submitdatetime": "2019-10-31 08:22:40" }, { "id": 3, "stuid": "17020031068", "username": "汪wqs", "phone": "17852417737", "applydate": "2019-10-31", "start": "09:45", "end": "10:45", "number": 7, "ifmedia": "是", "reason": "####", "status": "未还钥匙", "submitdatetime": "2019-10-31 08:22:40" }, { "id": 4, "stuid": "17020031068", "username": "汪wqs", "phone": "17852417737", "applydate": "2019-10-31", "start": "09:45", "end": "10:45", "number": 7, "ifmedia": "是", "reason": "####", "status": "未通过", "submitdatetime": "2019-10-31 08:22:40"}]'
 var app = getApp()
 Page({
   data: {
@@ -8,7 +8,6 @@ Page({
     all: [],
   },
   details: function (e) {
-    // var formID = e.target.dataset.formid;
     app.globalData.formId = e.target.dataset.formid;
     wx.navigateTo({
       url: "/pages/applyDetail/applyDetail"
@@ -31,20 +30,31 @@ Page({
       currentTab: e.detail.current,
     })
   },
-  onLoad: function (options) {
-    // 生命周期函数--监听页面加载 
-    this.setData({
-      all: JSON.parse(josn)
-    });
-    console.log(josn.length);
+  onShow:function(e){
+    var that = this;
     wx.request({
-      url: '',
-      data: {
-
+      url: 'http://132.232.121.52/A404_Server/AdminServlet?method=ShowAll',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' //传统POST方式
       },
       success: function (res) {
-        this.setData({
-          all: JSON.parse(res.data)
+        that.setData({
+          all: res.data
+        });
+      }
+    })
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载 
+    var that = this;
+    wx.request({
+      url: 'http://132.232.121.52/A404_Server/AdminServlet?method=ShowAll',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' //传统POST方式
+      },
+      success: function (res) {
+        that.setData({
+          all: res.data
         });
       }
     })
