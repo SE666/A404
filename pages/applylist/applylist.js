9e63f07c2e56a90a2e5aa1d52328c903a435af68
Nisces <Nisces0304@outlook.com>
2019-12-21 21:22:23 +0800

    update

--- pages/applylist/applylist.js
+++ pages/applylist/applylist.js
@@ -7,6 +7,26 @@ Page({
     applylist: []
   },
   onLoad: function () {
-
+    var that = this;
+    wx.showLoading({
+      title: '加载中...',
+      mask: true
+    });
+    wx.request({
+      url: 'http://132.232.121.52/A404_Server/FormServlet?method=findByUserid',
+      data: {
+        userid:1
+      },
+      method:'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' //传统POST方式
+      },
+      success: function (res) {
+        that.setData({
+          applylist: res.data
+        });
+        wx.hideLoading();
+      }
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -15,52 +15,21 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  condition: function(e) {
+    wx.navigateTo({
+      url: "/pages/schedule/schedule"
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  apply: function(e) {
+    wx.navigateTo({
+      url: '/pages/schedule/schedule',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  my: function(e) {
+    wx.navigateTo({
+      url: '/pages/applylist/applylist',
+    })
   }
 })
