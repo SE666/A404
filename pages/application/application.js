6fd815eb70f256b463af5c36a07ec58585cb88c2
Nisces <Nisces0304@outlook.com>
2019-12-21 18:20:22 +0800

    update

--- pages/application/application.js
+++ pages/application/application.js
@@ -17,90 +17,54 @@ Page({
   onLoad: function (options) {
     console.log(options);
     this.setData({
+      date: options.date,
       start: options.start,
       end: options.end
     })
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 申请表提交响应函数
    */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
   submitApplication: function (e) {
-    var userid = e.detail.value.UID;
+    var userid = e.detail.value.userid;
     var name = e.detail.value.name;
     var phone = e.detail.value.phone;
     var num = e.detail.value.num;
     var media = e.detail.value.media;
+    var reason = e.detail.value.reason;
     console.log(e.detail);
-    if (utils.isEmpty(UID) || utils.isEmpty(name) || utils.isEmpty(phone)
-      || utils.isEmpty(num) || utils.isEmpty(media)) {
+    if (utils.isEmpty(userid) || utils.isEmpty(name) || utils.isEmpty(phone)
+      || utils.isEmpty(num) || utils.isEmpty(media) || utils.isEmpty(reason)) {
       utils.alert("以上信息均不能为空！");
-      return false;
-    } else if (utils.isEmpty(name)) {
       return false;
     }
     var that = this;
     wx.request({
-      url: 'http://132.232.121.52/A404_Server/FormServlet?method=findById&id=1',
+      url: 'http://132.232.121.52/A404_Server/FormServlet?method=submit',
       data: {
-        "userid": userid,
-        "name": name,
-        "phone": phone,
-        "num": num,
-        "media": media
+        "userid": 1,
+        "applydate": that.data.date,
+        "start": that.data.start,
+        "end": that.data.end,
+        "number": num,
+        "ifmedia": media,
+        "reason": reason
       },
-      method: 'POST',
+      method: "POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' //传统POST方式
+      },
       success: function (res) {
-        console.log(res.data);
+        console.log(res);
+        if(res.data == "1") {
+          utils.alert("提交申请成功！");
+        } else {
+          utils.alert("提交申请失败！");
+        }
       },
       fail: function (res) {
-        console.log(res);
+        utils.alert("提交申请失败！");
       }
     })
   }
--- pages/schedule/schedule.js
+++ pages/schedule/schedule.js
@@ -2,63 +2,69 @@
 const utils = require('../../utils/util.js');
 var index = 0;
 var count = 0;
+var origin = null;  //原始表格数组数据
+var dateStart = null;
+var dateEnd = null;
 
-function init() {
-  getMondayDate();
-  var schedule = new Array(7);
-  for (let i = 0; i < 7; i++) {
-    schedule[i] = new Array(30);
-    for (let j = 0; j < 30; j++) {
-      schedule[i][j] = "";
-    }
+function formatDate(dateobj) {
+  var year = dateobj.getFullYear();
+  var month = dateobj.getMonth() + 1;
+  var date = dateobj.getDate();
+  if (month < 10) {
+    month = "0" + month;
   }
-  for (var i = 10; i < 15; i++) {
-    schedule[0][i] = "occupied";
+  if (date < 10) {
+    date = "0" + date;
   }
-  schedule[0][i] = "occupied last";
-
-  for (var i = 13; i < 20; i++) {
-    schedule[2][i] = "confirmed";
-  }
-  schedule[2][i] = "confirmed last";
-
-  for (var i = 4; i < 10; i++) {
-    schedule[4][i] = "confirmed";
-  }
-  schedule[4][i] = "confirmed last";
-
-  for (var i = 11; i < 17; i++) {
-    schedule[6][i] = "occupied";
-  }
-  schedule[6][i] = "occupied last";
-  return schedule;
+  return year + "-" + month + "-" + date;
 }
 
 /**
  * 获取离当前日期最近的星期一的日期
  */
-function getMondayDate() {
-  var date = new Date();
-  var day = date.getDay();
-  date.setDate(date.getDate() - day);
-  console.log(date.getDay());
+function getMonDate() {
+  var now = new Date();
+  var day = now.getDay();
+  var minus = day == 0 ? 6 : day - 1;
+  now.setDate(now.getDate() - minus);
+  return now;
 }
 
+/**
+ * 获取离当前日期最近的星期日的日期
+ */
+function getSunDate() {
+  var now = new Date();
+  var day = now.getDay();
+  var plus = day == 0 ? 0 : 7 - day;
+  now.setDate(now.getDate() + plus);
+  return now;
+}
+
+/**
+ * 当前单元格是否被选中
+ */
 function isSelected(cell) {
   return cell == "selected" || cell == "selected last";
 }
 
+/**
+ * 获取所选时间段的开始时间
+ */
 function getStart() {
   var start = [];
   if (count == 0) {
     start = ["00", "00"];
     return start;
   }
-  start[0] = ("0" + (parseInt(index / 2) + 8)).slice(-2);  //0+时间，取后两位，用于不足位数补0
+  start[0] = ("0" + (parseInt(index / 2) + 8)).slice(-2); //0+时间，取后两位，用于不足位数补0
   start[1] = index % 2 == 0 ? "00" : "30";
   return start;
 }
 
+/**
+ * 获取所选时间段的结束时间
+ */
 function getEnd() {
   var end = [];
   if (count == 0) {
@@ -66,81 +72,86 @@ function getEnd() {
     return end;
   }
   var eIndex = index + count;
-  end[0] = ("0" + (parseInt(eIndex / 2) + 8)).slice(-2);  //0+时间，取后两位，用于不足位数补0
+  end[0] = ("0" + (parseInt(eIndex / 2) + 8)).slice(-2); //0+时间，取后两位，用于不足位数补0
   end[1] = eIndex % 2 == 0 ? "00" : "30";
   return end;
 }
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     schedule: [],
     start: ["00", "00"],
-    end: ["00", "00"]
+    end: ["00", "00"],
+    dateSelected: "00-00-00",
+    dateStart: "00-00-00",
+    dateEnd: "00-00-00",
+    next: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
+    var start = getMonDate();
+    var end = getSunDate();
+    dateStart = start;
+    dateEnd = end;
+    this.getSchedule(start, end);
+  },
+
+  getSchedule: function (start, end) {
+    var dateStart = formatDate(start);
+    var dateEnd = formatDate(end);
+    var that = this;
+    wx.request({
+      url: 'http://132.232.121.52/A404_Server/FormServlet?method=schedule',
+      data: {
+        dateStart: dateStart,
+        dateEnd: dateEnd
+      },
+      method: "POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' //传统POST方式
+      },
+      success: function (res) {
+        origin = JSON.stringify(res.data); //保存原始数组的Json供重置时使用
+        that.setData({
+          schedule: res.data,
+          dateStart: dateStart,
+          dateEnd: dateEnd
+        });
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    })
+  },
+
+  nextweek: function () {
+    dateStart.setDate(dateStart.getDate() + 7);
+    dateEnd.setDate(dateEnd.getDate() + 7);
+    this.getSchedule(dateStart, dateEnd);
     this.setData({
-      schedule: init()
-    });
+      next: true
+    })
+  },
+
+  prevweek: function () {
+    dateStart.setDate(dateStart.getDate() - 7);
+    dateEnd.setDate(dateEnd.getDate() - 7);
+    this.getSchedule(dateStart, dateEnd);
+    this.setData({
+      next: false
+    })
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 单元格被点击响应函数
    */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  },
-
-  onCellTap: function(e) {
+  onCellTap: function (e) {
     var col = e.currentTarget.dataset.col;
     var row = e.currentTarget.dataset.row;
     var schedule = this.data.schedule;
@@ -154,6 +165,11 @@ Page({
       }
     } else {
       index = row;
+      var dateSelected = new Date();
+      dateSelected.setDate(dateStart.getDate() + col);
+      this.setData({
+        dateSelected: formatDate(dateSelected)
+      })
     }
     if (schedule[col][row] == "") {
       schedule[col][row] = "selected";
@@ -188,27 +204,34 @@ Page({
     });
   },
 
-  submit: function(e) {
-    if(count == 0) {
+  /**
+   * 提交按钮点击响应函数
+   */
+  submit: function (e) {
+    if (count == 0) {
       utils.alert("请选择时间！");
       return;
     }
     var start = this.data.start;
     var end = this.data.end;
+    var date = this.data.dateSelected;
     var startTime = start[0] + ":" + start[1];
     var endTime = end[0] + ":" + end[1];
     wx.navigateTo({
-      url:"/pages/application/application?start=" + startTime + "&end=" + endTime
+      url: "/pages/application/application?date=" + date + "&start=" + startTime + "&end=" + endTime
     })
   },
 
-  reset:function(e) {
+  /**
+   * 重置按钮点击响应函数
+   */
+  reset: function (e) {
     index = 0;
     count = 0;
     this.setData({
-      schedule: init(),
-      start:["00", "00"],
-      end:["00", "00"]
+      schedule: JSON.parse(origin),
+      start: ["00", "00"],
+      end: ["00", "00"]
     });
   }
 })
